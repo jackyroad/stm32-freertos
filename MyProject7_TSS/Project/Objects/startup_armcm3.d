8a2c981ad27f41b3d935ef2c090545db88523d18
jackyroad <13762713527@qq.com>
2025-04-26 11:30:16 +0800

    test4

--- MyProject7_TSS/Project/Objects/startup_armcm3.d
+++
@@ -1,8 +0,0 @@
-.\objects\startup_armcm3.o: RTE/Device/ARMCM3/startup_ARMCM3.c
-.\objects\startup_armcm3.o: D:\software\Keil_v5\ARM\Packs\ARM\Cortex_DFP\1.1.0\Device\ARMCM3\Include\ARMCM3.h
-.\objects\startup_armcm3.o: D:\software\Keil_v5\ARM\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm3.h
-.\objects\startup_armcm3.o: D:\software\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\startup_armcm3.o: D:\software\Keil_v5\ARM\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\cmsis_version.h
-.\objects\startup_armcm3.o: D:\software\Keil_v5\ARM\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\cmsis_compiler.h
-.\objects\startup_armcm3.o: D:\software\Keil_v5\ARM\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\m-profile/armv7m_mpu.h
-.\objects\startup_armcm3.o: D:\software\Keil_v5\ARM\Packs\ARM\Cortex_DFP\1.1.0\Device\ARMCM3\Include\system_ARMCM3.h
--- MyProject7_TSS/Project/Objects/system_armcm3.d
+++
@@ -1,8 +0,0 @@
-.\objects\system_armcm3.o: RTE/Device/ARMCM3/system_ARMCM3.c
-.\objects\system_armcm3.o: D:\software\Keil_v5\ARM\Packs\ARM\Cortex_DFP\1.1.0\Device\ARMCM3\Include\ARMCM3.h
-.\objects\system_armcm3.o: D:\software\Keil_v5\ARM\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm3.h
-.\objects\system_armcm3.o: D:\software\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\system_armcm3.o: D:\software\Keil_v5\ARM\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\cmsis_version.h
-.\objects\system_armcm3.o: D:\software\Keil_v5\ARM\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\cmsis_compiler.h
-.\objects\system_armcm3.o: D:\software\Keil_v5\ARM\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\m-profile/armv7m_mpu.h
-.\objects\system_armcm3.o: D:\software\Keil_v5\ARM\Packs\ARM\Cortex_DFP\1.1.0\Device\ARMCM3\Include\system_ARMCM3.h
